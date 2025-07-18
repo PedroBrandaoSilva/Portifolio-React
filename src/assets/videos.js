017cbbd9944c4246f170c5c548d2ee854046590b
PedroBrandaoSilva <redpig825@gmail.com>
2025-07-18 11:50:42 -0300

    bug dir videos

--- src/assets/videos.js
+++ src/assets/videos.js
@@ -1,6 +1,6 @@
 import calculator from './videos/Calculator.mp4';
 import coachPage from './videos/coachPage.mp4';
-import jokenPo from './videos/jokenPo.mp4';
+import jokenPo from './videos/JokenPo.mp4';
 import layoutsWeb from './videos/Layouts.mp4';
 import cadastroUsuarios from './videos/CadastroUsuarios.mp4';
 import gerenciadorDeUsuarios from './videos/gerenciadorDeUsuarios.mp4';
